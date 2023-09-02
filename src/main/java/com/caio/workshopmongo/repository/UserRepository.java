8f5247ed199906331c06a1582fc06871a75f9d81
CaioSouzaAds <caio.cesarads@gmail.com>
2023-09-02 10:40:50 -0300

    connecting to MongoDb with repository and service

---
+++ src/main/java/com/caio/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.caio.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.caio.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
