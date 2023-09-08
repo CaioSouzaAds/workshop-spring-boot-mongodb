250a19dc1b0888ee1763be19719860f0bf00026f
CaioSouzaAds <caio.cesarads@gmail.com>
2023-09-08 10:37:49 -0300

    getting User by ID

--- src/main/java/com/caio/workshopmongo/services/UserService.java
+++ src/main/java/com/caio/workshopmongo/services/UserService.java
@@ -1,20 +1,32 @@
 package com.caio.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.caio.workshopmongo.domain.User;
 import com.caio.workshopmongo.repository.UserRepository;
+import com.caio.workshopmongo.services.excepition.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		if (!user.isPresent()) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		} else {
+			return user.get();
+		}
+
 	}
 }
